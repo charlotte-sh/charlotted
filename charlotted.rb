526676eeab247723e771d405dc706603bcc01a28
Jura <hello@mynameisjura.com>
2020-08-08 17:47:03 +0300

    send workspace name, account name and online members on connect

--- charlotted.rb
+++ charlotted.rb
@@ -2,10 +2,11 @@ require 'socket'
 
 class Server
   PORT = 1654
+  WORKSPACE = 'Charlotte'
 
-	def initialize
-		@server = TCPServer.open(PORT)
-		@clients = {}
+  def initialize
+    @server = TCPServer.open(PORT)
+    @clients = {}
     run
   end
 
@@ -13,33 +14,49 @@ class Server
 
   def run
     Socket.accept_loop(@server) do |connection|
-			Thread.new { handle(connection) }
+      Thread.new { handle(connection) }
     end
-	end
+  end
 
   def handle(connection)
-  	loop do
+    loop do
       if connection.eof?
         on_disconnect(connection)
       elsif @clients[connection].nil?
         on_handshake(connection)
       else
-  			on_message(connection)
+        on_message(connection)
       end
     end
   end
 
   def on_handshake(connection)
-		user_id = connection.gets.chomp
-		@clients[connection] = user_id
+    user_id = connection.gets.chomp
+    @clients[connection] = user_id
+
+    online_users = @clients.values - [user_id]
+    connection.puts "Workspace: #{WORKSPACE}"
+    connection.puts "Account:   #{user_id}"
+    connection.puts "Online:    #{online_users.join(', ')}"
+    connection.puts
     puts "#{@clients[connection]} connected"
   end
 
-	def on_message(connection)
+  def on_message(connection)
     message = connection.gets.chomp
-		@clients.each { |connection, user_id| connection.puts "#{user_id}: #{message}" }
-    puts "#{@clients[connection]} sent a message"
-	end
+
+    case message
+    when /\/.*/
+    when '/machines'
+      connection.puts @clients.values.map { |x| x.dup.prepend '- ' }
+      connection.puts
+    else
+      unless message.empty?
+        @clients.each { |client, user_id| client.puts "#{@clients[connection]}: #{message}" }
+        puts "#{@clients[connection]} sent a message"
+      end
+    end
+  end
 
   def on_disconnect(connection)
     unless @clients[connection].nil?
